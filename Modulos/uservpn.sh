a47d8f45fed0a406b78510840a13862847e6c189
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-06-19 10:36:02 +0700

    Update uservpn.sh

--- Modulos/uservpn.sh
+++ Modulos/uservpn.sh
@@ -4,13 +4,13 @@ IP=$(cat /etc/IP)
 newclient () {
   cp /etc/openvpn/client-common.txt ~/$1.ovpn
   echo "<ca>" >> ~/$1.ovpn
-  cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
+  cat /etc/openvpn/ca.crt >> ~/$1.ovpn
   echo "</ca>" >> ~/$1.ovpn
   echo "<cert>" >> ~/$1.ovpn
-  cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
+  cat /etc/openvpn/client.crt >> ~/$1.ovpn
   echo "</cert>" >> ~/$1.ovpn
   echo "<key>" >> ~/$1.ovpn
-  cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
+  cat /etc/openvpn/client.key >> ~/$1.ovpn
   echo "</key>" >> ~/$1.ovpn
   echo "<tls-auth>" >> ~/$1.ovpn
   cat /etc/openvpn/ta.key >> ~/$1.ovpn
