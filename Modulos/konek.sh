09a49e4703176cb1923a0dd91770b7f6241ec469
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-06-19 09:52:46 +0700

    Update konek.sh

--- Modulos/konek.sh
+++ Modulos/konek.sh
@@ -583,10 +583,10 @@ newclient () {
 	cat /etc/openvpn/ca.crt >> ~/$1.ovpn
 	echo "</ca>" >> ~/$1.ovpn
 	echo "<cert>" >> ~/$1.ovpn
-	cat /etc/openvpn/$1.crt >> ~/$1.ovpn
+	cat /etc/openvpn/client.crt >> ~/$1.ovpn
 	echo "</cert>" >> ~/$1.ovpn
 	echo "<key>" >> ~/$1.ovpn
-	cat /etc/openvpn/$1.key >> ~/$1.ovpn
+	cat /etc/openvpn/client.key >> ~/$1.ovpn
 	echo "</key>" >> ~/$1.ovpn
 	echo "<tls-auth>" >> ~/$1.ovpn
 	cat /etc/openvpn/ta.key >> ~/$1.ovpn
