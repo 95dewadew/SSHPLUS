a5f35f06b401808d08f2c52e2936d782162cbb9f
95dewadew <41128556+95dewadew@users.noreply.github.com>
2020-07-05 03:30:01 +0700

    Update konek.sh

--- Modulos/konek.sh
+++ Modulos/konek.sh
@@ -120,31 +120,42 @@ echo ".claro.com.br/
 .tim.com.br/
 .vivo.com.br/
 .oi.com.br/" > $var_pay
-echo "acl url1 dstdomain -i 127.0.0.1
-acl url2 dstdomain -i localhost
-acl url3 dstdomain -i $ipdovps
-acl url4 dstdomain -i /SSHPLUS?
-acl payload url_regex -i "$var_pay"
-acl all src 0.0.0.0/0
+echo "#acl manager proto cache_object
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst $ipdovps-$ipdovps/255.255.255.255
+http_access allow SSH
+http_access allow manager localhost
+http_access deny manager
+http_access allow localhost
+http_access deny all
 
-http_access allow url1
-http_access allow url2
-http_access allow url3
-http_access allow url4
-http_access allow payload
-http_access allow all
  
-#Portas" > $var_sqd
+#Port" > $var_sqd
 for Pts in $(echo -e $portass); do
 echo -e "http_port $Pts" >> $var_sqd
 [[ -f "/usr/sbin/ufw" ]] && ufw allow $Pts/tcp
 done
 echo -e "
-#Nome squid
-visible_hostname SSHPLUS 
-via off
-forwarded_for off
-pipeline_prefetch off" >> $var_sqd
+#Name squid
+coredump_dir /var/spool/squid3
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname SSH" >> $var_sqd
 sqd_conf () {
 if [[ -d "/etc/squid/" ]]; then
 squid -k reconfigure
